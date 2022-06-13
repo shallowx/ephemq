ac3b6e77cde8b671fcf40f5b6430fa9a612e2017
presenth7s <present.h7s@gmail.com>
2022-06-13 19:02:30 +0800

    init shallow-tool framework support

---
+++ shallow-tool/src/main/java/org/shallow/ShallowAdmin.java
@@ -0,0 +1,7 @@
+package org.shallow;
+
+public class ShallowAdmin {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ shallow-tool/src/test/java/org/shallow/ShallowAdminTest.java
@@ -0,0 +1,13 @@
+package org.shallow;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ShallowAdminTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
