34c27a265b3240511cad4879785838617044dd9a
presenth7s <present.h7s@gmail.com>
2022-06-19 19:25:57 +0800

    init shallow framework support

---
+++ shallow-common/src/main/java/org/shallow/StringUtils.java
@@ -0,0 +1,20 @@
+package org.shallow;
+
+public final class StringUtils {
+
+    public boolean isBlank(String content) {
+        return false;
+    }
+
+    public boolean isNotBlank(String content) {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isNotEmpty() {
+        return false;
+    }
+}
