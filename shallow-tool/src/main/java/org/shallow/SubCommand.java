35763b656fcd67682589a9bc28ea2a36d1b5e20e
presenth7s <present.h7s@gmail.com>
2022-06-28 11:30:47 +0800

    init shallow framework support

--- shallow-tool/src/main/java/org/shallow/SubCommand.java
+++ shallow-tool/src/main/java/org/shallow/SubCommand.java
@@ -1,4 +1,11 @@
 package org.shallow;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
 public interface SubCommand {
+    String name();
+    String desc();
+    Options buildOptions(final Options options);
+    void execute(final CommandLine cmdLine, final Options options) throws Exception;
 }
