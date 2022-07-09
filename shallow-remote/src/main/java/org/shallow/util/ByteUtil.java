d33dfaa9b104c123af4615027e05102adb739482
presenth7s <present.h7s@gmail.com>
2022-07-09 21:08:13 +0800

    init shallow framework support for shallow data protocol

--- shallow-remote/src/main/java/org/shallow/util/ByteUtil.java
+++ shallow-remote/src/main/java/org/shallow/util/ByteUtil.java
@@ -1,4 +1,29 @@
 package org.shallow.util;
 
+import io.netty.buffer.ByteBuf;
+
+import java.nio.charset.StandardCharsets;
+
 public final class ByteUtil {
+    private ByteUtil() {}
+
+    public static String byteToString(ByteBuf buf, int maxLength) {
+        if (buf == null) {
+            return null;
+        }
+        int length = buf.readableBytes();
+        length = maxLength < 0 ? length : Math.min(maxLength, length);
+        try {
+            return buf.toString(buf.readerIndex(), length, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static boolean release(ByteBuf buf) {
+        if (buf == null) {
+            return Boolean.FALSE;
+        }
+        return buf.release();
+    }
 }
