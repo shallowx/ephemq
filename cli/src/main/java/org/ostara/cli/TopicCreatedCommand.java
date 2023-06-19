1106348e40904ca30ed5ceeadc2a417db1695979
bodhili <present.h7s@gmail.com>
2023-06-19 11:41:05 +0800

    update command line tools

--- cli/src/main/java/org/ostara/cli/TopicCreatedCommand.java
+++ cli/src/main/java/org/ostara/cli/TopicCreatedCommand.java
@@ -1,27 +1,180 @@
 package org.ostara.cli;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import io.netty.util.internal.StringUtil;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
 import org.ostara.client.internal.Client;
+import org.ostara.client.internal.ClientChannel;
+import org.ostara.common.TopicConfig;
+import org.ostara.common.util.StringUtils;
+import org.ostara.remote.proto.server.CreateTopicResponse;
+import org.ostara.remote.proto.server.PartitionsReplicas;
+import org.ostara.remote.util.NetworkUtils;
+
+import java.io.File;
+import java.net.SocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TopicCreatedCommand implements Command {
     @Override
     public String name() {
-        return null;
+        return "createTopic";
     }
 
     @Override
     public String description() {
-        return null;
+        return "create topic";
     }
 
     @Override
     public Options buildOptions(Options options) {
-        return null;
+        Option brokerOpt = new Option("b", "broker address", true, "which broker server");
+        brokerOpt.setRequired(true);
+        options.addOption(brokerOpt);
+
+        Option partitionOpt = new Option("p", "partition", true, "partition number");
+        partitionOpt.setRequired(true);
+        options.addOption(partitionOpt);
+
+        Option replicaOpt = new Option("r", "partition", true, "replica number");
+        partitionOpt.setRequired(true);
+        options.addOption(replicaOpt);
+
+        Option configOpt = new Option("s", "topic config", true, "topic config(JSON)");
+        partitionOpt.setRequired(true);
+        options.addOption(configOpt);
+
+        Option explainOpt = new Option("e", "explain file", true, "topic to which file(JSON)");
+        partitionOpt.setRequired(true);
+        options.addOption(explainOpt);
+        return options;
     }
 
     @Override
     public void execute(CommandLine commandLine, Options options, Client client) throws Exception {
+        try {
+            if (commandLine.hasOption('b')) {
+                if (commandLine.hasOption('e')) {
+                    String explainFile = commandLine.getOptionValue('e');
+                    if (!StringUtils.isNullOrEmpty(explainFile)) {
+                        String content = FileUtils.readFileToString(new File(explainFile), StandardCharsets.UTF_8);
+                        Gson gson = new Gson();
+                        List<TopicMetadata> lists = gson.fromJson(content, new TypeToken<List<TopicMetadata>>() {}.getType());
+                        if (lists == null || lists.isEmpty()) {
+                          return;
+                        }
+                         for (TopicMetadata metadata : lists) {
+                             String topic = metadata.topic;
+                             int partition = metadata.partition;
+                             int replicas = metadata.replicas;
+                             TopicConfig config = metadata.config;
+                             CreateTopicResponse response = client.createTopic(topic, partition, replicas, config);
+                             String print = print(response, topic, partition, replicas, config);
+                             System.out.printf("%s [%s] INFO %s - %s \n", newDate(), Thread.currentThread().getName(), TopicListCommand.class.getName(), print);
+                         }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.printf("%s [%s] ERROR %s - %s \n", newDate(), Thread.currentThread().getName(), TopicListCommand.class.getName(), e.getCause().getMessage());
+        }
+    }
 
+    private String print(CreateTopicResponse response, String topic, int partition, int replica, TopicConfig config) {
+        TopicMetadata metadata = new TopicMetadata();
+        metadata.topic = topic;
+        metadata.config = config;
+        metadata.partition = partition;
+        metadata.replicas = replica;
+
+        List<PartitionsReplicas> partitionsReplicasList = response.getPartitionsReplicasList();
+        metadata.partitions = partitionsReplicasList.stream()
+                .collect(Collectors.toMap(
+                        PartitionsReplicas::getPartition, t -> new ArrayList<>(t.getReplicasList()
+                        )));
+        Gson gSon = new Gson();
+        return gSon.toJson(metadata);
+    }
+
+    private static class TopicMetadata{
+        private String topic;
+        private int partition;
+        private int replicas;
+        private TopicConfig config;
+        private Map<Integer, List<String>> partitions;
+
+        public TopicMetadata() {
+        }
+
+        public TopicMetadata(String topic, int partition, int replicas, TopicConfig config, Map<Integer, List<String>> partitions) {
+            this.topic = topic;
+            this.partition = partition;
+            this.replicas = replicas;
+            this.config = config;
+            this.partitions = partitions;
+        }
+
+        public String getTopic() {
+            return topic;
+        }
+
+        public void setTopic(String topic) {
+            this.topic = topic;
+        }
+
+        public int getPartition() {
+            return partition;
+        }
+
+        public void setPartition(int partition) {
+            this.partition = partition;
+        }
+
+        public int getReplicas() {
+            return replicas;
+        }
+
+        public void setReplicas(int replicas) {
+            this.replicas = replicas;
+        }
+
+        public TopicConfig getConfig() {
+            return config;
+        }
+
+        public void setConfig(TopicConfig config) {
+            this.config = config;
+        }
+
+        public Map<Integer, List<String>> getPartitions() {
+            return partitions;
+        }
+
+        public void setPartitions(Map<Integer, List<String>> partitions) {
+            this.partitions = partitions;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "topic='" + topic + '\'' +
+                    ", partition=" + partition +
+                    ", replicas=" + replicas +
+                    ", config=" + config +
+                    ", partitions=" + partitions +
+                    '}';
+        }
+    }
+
+    private static String newDate() {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss.SSS");
+        return   format.format(new Date());
     }
 }
--- cli/src/main/java/org/ostara/cli/TopicListCommand.java
+++ cli/src/main/java/org/ostara/cli/TopicListCommand.java
@@ -1,18 +1,26 @@
 package org.ostara.cli;
 
+import com.google.gson.Gson;
+import com.google.protobuf.ProtocolStringList;
 import io.netty.util.internal.StringUtil;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.ostara.client.internal.Client;
 import org.ostara.client.internal.ClientChannel;
+import org.ostara.remote.proto.PartitionMetadata;
 import org.ostara.remote.proto.TopicInfo;
 import org.ostara.remote.util.NetworkUtils;
 
 import java.net.SocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class TopicListCommand implements Command{
+public class TopicListCommand implements Command {
     @Override
     public String name() {
         return "TopicList";
@@ -58,24 +66,129 @@ public class TopicListCommand implements Command{
                 }
                 SocketAddress socketAddress = NetworkUtils.switchSocketAddress(address);
                 ClientChannel clientChannel = client.fetchChannel(socketAddress);
-
+                List<TopicMetadata> topics = new ArrayList<>();
                 if (commandLine.hasOption("t")) {
                     String topic = commandLine.getOptionValue("t").trim();
                     Map<String, TopicInfo> topicInfos = StringUtil.isNullOrEmpty(topic)
                             ? client.queryTopicInfos(clientChannel, topic)
                             : client.queryTopicInfos(clientChannel);
 
-                    if (topicInfos.isEmpty()) {
-                        System.out.print("Topic info is empty");
+                    if (topicInfos == null || topicInfos.isEmpty()) {
+                        System.out.printf("%s [%s] INFO %s - topic info is empty \n", newDate(), Thread.currentThread().getName(), TopicListCommand.class.getName());
                         return;
                     }
+                    topics = topicInfos.values().stream().map(topicInfo -> {
+                        Map<Integer, PartitionMetadata> partitionsMap = topicInfo.getPartitionsMap();
+                        if (!partitionsMap.isEmpty()) {
+                            return partitionsMap.values().stream().map(pm -> {
+                                ProtocolStringList replicaNodeIdsList = pm.getReplicaNodeIdsList();
+                                List<String> replicaNodeIds = new ArrayList<>(replicaNodeIdsList);
 
-                    System.out.println(topicInfos);
+                                return new TopicMetadata(topicInfo.getTopic().getName(), pm.getId(), pm.getLedger(), pm.getEpoch(), pm.getLeaderNodeId(), replicaNodeIds);
+                            }).findAny().get();
+                        }
+                        return null;
+                    }).toList();
                 }
-            }
 
+                if (commandLine.hasOption('l')) {
+                    int ledger = Integer.parseInt(commandLine.getOptionValue('l'));
+                    topics = topics.stream().filter(t -> t.getLedger() == ledger).toList();
+                }
+
+                if (commandLine.hasOption('p')) {
+                    int partition = Integer.parseInt(commandLine.getOptionValue('p'));
+                    topics = topics.stream().filter(t -> t.getPartition() == partition).toList();
+                }
+
+                Gson gson = new Gson();
+                System.out.printf("%s [%s] INFO %s - %s \n", newDate(), Thread.currentThread().getName(), TopicListCommand.class.getName(), gson.toJson(topics));
+            }
         } catch (Throwable t){
-            System.out.println(t.getMessage());
+            System.out.printf("%s [%s] ERROR %s - %s \n", newDate(), Thread.currentThread().getName(), TopicListCommand.class.getName(), t.getCause().getMessage());
         }
+    }
+
+    private static class TopicMetadata {
+        private String topic;
+        private int partition;
+        private int ledger;
+        private int epoch;
+        private String leader;
+        private List<String> replicas;
+
+        public TopicMetadata(String topic, int partition, int ledger, int epoch, String leader, List<String> replicas) {
+            this.topic = topic;
+            this.partition = partition;
+            this.ledger = ledger;
+            this.epoch = epoch;
+            this.leader = leader;
+            this.replicas = replicas;
+        }
+
+        public String getTopic() {
+            return topic;
+        }
+
+        public void setTopic(String topic) {
+            this.topic = topic;
+        }
+
+        public int getPartition() {
+            return partition;
+        }
+
+        public void setPartition(int partition) {
+            this.partition = partition;
+        }
+
+        public int getLedger() {
+            return ledger;
+        }
+
+        public void setLedger(int ledger) {
+            this.ledger = ledger;
+        }
+
+        public int getEpoch() {
+            return epoch;
+        }
+
+        public void setEpoch(int epoch) {
+            this.epoch = epoch;
+        }
+
+        public String getLeader() {
+            return leader;
+        }
+
+        public void setLeader(String leader) {
+            this.leader = leader;
+        }
+
+        public List<String> getReplicas() {
+            return replicas;
+        }
+
+        public void setReplicas(List<String> replicas) {
+            this.replicas = replicas;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "topic='" + topic + '\'' +
+                    ", partition=" + partition +
+                    ", ledger=" + ledger +
+                    ", epoch=" + epoch +
+                    ", leader='" + leader + '\'' +
+                    ", replicas=" + replicas +
+                    '}';
+        }
+    }
+
+    private static String newDate() {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss.SSS");
+        return   format.format(new Date());
     }
 }
