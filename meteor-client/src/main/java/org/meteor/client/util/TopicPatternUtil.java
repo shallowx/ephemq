7ba2e7fa1e98c23a90e31d5b1392cfbf4da0306d
bodhili <present.h7s@gmail.com>
2024-02-10 21:04:01 +0800

    review code

--- meteor-client/src/main/java/org/meteor/client/util/TopicPatternUtil.java
+++ meteor-client/src/main/java/org/meteor/client/util/TopicPatternUtil.java
@@ -6,7 +6,7 @@ public class TopicPatternUtil {
     private static final Pattern TOPIC_PATTERN = Pattern.compile("^[\\w\\-#]+$");
     public static void validateTopic(String topic) {
         if (topic == null) {
-            throw new IllegalArgumentException("Topic not found");
+            throw new NullPointerException("Topic cannot be empty");
         }
 
         if (!TOPIC_PATTERN.matcher(topic).matches()) {
@@ -16,19 +16,19 @@ public class TopicPatternUtil {
 
     public static void validatePartition(int number) {
         if (number <= 0) {
-            throw new IllegalArgumentException(String.format("Partition[%d] limit should be > 0", number));
+            throw new IllegalArgumentException(String.format("Partition limit[%d] should be > 0", number));
         }
     }
 
     public static void validateLedgerReplica(int number) {
         if (number <= 0) {
-            throw new IllegalArgumentException(String.format("Ledger replicas[%d] limit should be > 0", number));
+            throw new IllegalArgumentException(String.format("Ledger replicas limit[%d] should be > 0", number));
         }
     }
 
     public static void validateQueue(String queue) {
         if (queue == null) {
-            throw new IllegalArgumentException("Topic queue not found");
+            throw new NegativeArraySizeException("Topic queue cannot be empty");
         }
     }
 }
