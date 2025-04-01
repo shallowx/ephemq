4c7ee0f3e78c9984ab26b1546ae8d3872d05c6d8
jiuyi.dream <jiuyi.dream@gmail.com>
2025-04-01 21:23:47 +0800

    update proxy unit test

--- meteor-proxy/src/test/java/org/meteor/proxy/core/ProxyConfigTests.java
+++ meteor-proxy/src/test/java/org/meteor/proxy/core/ProxyConfigTests.java
@@ -1,4 +1,24 @@
 package org.meteor.proxy.core;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.meteor.config.CommonConfig;
+import org.meteor.config.ZookeeperConfig;
+
+import java.util.Properties;
+
 public class ProxyConfigTests {
+
+    @Test
+    public void testProxyConfig() {
+        Properties prop = new Properties();
+        prop.put("proxy.upstream.servers", "127.0.0.1:10000");
+        CommonConfig commonConfiguration = new CommonConfig(prop);
+        ZookeeperConfig zookeeperConfiguration = new ZookeeperConfig(prop);
+        ProxyConfig proxyConfig = new ProxyConfig(prop, commonConfiguration, zookeeperConfiguration);
+        Assert.assertEquals(commonConfiguration, proxyConfig.getCommonConfiguration());
+        Assertions.assertEquals("127.0.0.1:10000", proxyConfig.getProxyUpstreamServers());
+        Assertions.assertEquals(200000, proxyConfig.getProxyHeavyLoadSubscriberThreshold());
+    }
 }
--- meteor-proxy/src/test/java/org/meteor/proxy/core/ProxyServerConfigTests.java
+++ meteor-proxy/src/test/java/org/meteor/proxy/core/ProxyServerConfigTests.java
@@ -1,4 +1,20 @@
 package org.meteor.proxy.core;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Properties;
+
 public class ProxyServerConfigTests {
+
+    @Test
+    public void testProxyServerConfig() {
+        Properties properties = new Properties();
+        properties.put("proxy.upstream.servers", "127.0.0.1:10000");
+        ProxyServerConfig proxyServerConfig = new ProxyServerConfig(properties);
+        Assertions.assertNotNull(proxyServerConfig);
+        Assertions.assertNotNull(proxyServerConfig.getProxyConfiguration());
+        Assertions.assertEquals("127.0.0.1:10000", proxyServerConfig.getProxyConfiguration().getProxyUpstreamServers());
+        Assertions.assertEquals(200000, proxyServerConfig.getProxyConfiguration().getProxyHeavyLoadSubscriberThreshold());
+    }
 }
