4a49c1d8e6f1e56e9ed227e7e82e7b1cc59addfd
jiuyi.dream <jiuyi.dream@gmail.com>
2025-04-03 13:48:10 +0800

    update proxy unit test

---
+++ meteor-proxy/src/test/java/org/meteor/proxy/core/ProxyLogTests.java
@@ -0,0 +1,31 @@
+package org.meteor.proxy.core;
+
+import io.netty.util.concurrent.ImmediateEventExecutor;
+import io.netty.util.concurrent.Promise;
+import org.junit.Assert;
+import org.junit.Test;
+import org.meteor.common.message.TopicConfig;
+import org.meteor.common.message.TopicPartition;
+import org.meteor.support.DefaultMeteorManager;
+import org.meteor.support.Manager;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProxyLogTests {
+
+    @Test
+    public void testCancelSyncAndCloseIfNotSubscribe() throws ExecutionException, InterruptedException {
+        Properties prop = new Properties();
+        ProxyServerConfig proxyServerConfig = new ProxyServerConfig(prop);
+        TopicPartition topicPartition = new TopicPartition("test", 0);
+        Manager manager = new DefaultMeteorManager(proxyServerConfig);
+        TopicConfig topicConfig = new TopicConfig();
+
+        ProxyLog proxyLog = new ProxyLog(proxyServerConfig, topicPartition, 1, 1, manager, topicConfig);
+        Promise<Boolean> promise = ImmediateEventExecutor.INSTANCE.newPromise();
+        proxyLog.cancelSyncAndCloseIfNotSubscribe(promise);
+        Assert.assertFalse(promise.isSuccess());
+        Assert.assertTrue(promise.get());
+    }
+}
